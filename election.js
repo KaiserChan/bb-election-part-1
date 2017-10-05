4a60d07528aa201b22ef4fe4fc2f424e23ae1ecf
KaiserChan <chankaiserkk@gmail.com>
2017-10-05 12:53:26 -0400

    updated bb-election-part-1 ... updated election.js file with ajax call and looping response data and showing results in html via DOM manipulation

--- election.js
+++ election.js
@@ -1,5 +1,34 @@
 document.addEventListener("DOMContentLoaded", function() {
 
   // Imagination!
+  var allCandidates = document.querySelector('#all-candidates');
+
+  $.ajax ({
+      url: "https://bb-election-api.herokuapp.com/",
+      method: "GET",
+      dataType: "JSON"
+    }).done(function (data) {
+
+      var candidates = data['candidates']
+      for (i = 0; i < candidates.length; i++) {
+        var candidate = document.createElement('div');
+
+        var name = document.createElement('li');
+        name.setAttribute('class', 'candidate-name');
+
+        var votes = document.createElement('li');
+        votes.setAttribute('class', 'candidate-vote');
+
+        name.innerHTML = 'Name: ' + candidates[i]['name'];
+        votes.innerHTML = ' Votes: ' + candidates[i]['votes'];
+
+        var lineBreak = document.createElement('br');
+
+        candidate.appendChild(name);
+        candidate.appendChild(votes);
+        allCandidates.appendChild(candidate);
+        allCandidates.appendChild(lineBreak);
+      }
+    });
 
 });
